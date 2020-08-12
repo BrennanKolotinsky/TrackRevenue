22f7ee0fff7577ab579bf4874d2253ddacf05763
Brennan Kolotinsky <brennankolotinsky@Brennans-MacBook-Pro.local>
2020-08-12 18:57:21 -0400

    added the headers

--- classes/createPriv.php
+++ classes/createPriv.php
@@ -2,7 +2,7 @@
 
 class createPriv {
 
-	private $PATH_TO_JSON = "priv.json";
+	private $PATH_TO_JSON = "inputs/priv.json";
 	private $PATH_TO_WRITE = "outputs/priv.csv";
 
 	public function __construct() {
@@ -18,5 +18,24 @@ class createPriv {
 	public function writeCSV(Array $json, String $PATH_TO_WRITE) {
 		print_r($json);
 		$output = fopen($PATH_TO_WRITE, "w");
+		fputcsv($output, $this->createHeaders());
+
+		fclose($output);
+	}
+
+	public function createHeaders() {
+		$array = array(
+    		0 => "",
+    		1 => "view_grades",
+    		2 => "change_grades",
+    		3 => "add_grades",
+    		4 => "delete_grades",
+    		5 => "view_classes",
+    		6 => "change_classes",
+    		7 => "add_classes",
+    		8 => "delete_classes",
+		);
+
+		return $array;
 	}
 }
