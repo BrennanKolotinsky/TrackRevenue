dc46d81ff04b8606830ab68c8776ec43e45ed620
Brennan Kolotinsky <brennankolotinsky@Brennans-MacBook-Pro.local>
2020-08-12 16:56:27 -0400

    more structure

--- classes/createPriv.php
+++ classes/createPriv.php
@@ -2,14 +2,20 @@
 
 class createPriv {
 
-	private $PATH_TO_JSON = "priv.json";
+	private $PATH_TO_JSON = "../inputs/priv.json";
+	//private $PATH_TO_WRITE = "../outputs/priv.csv";
 
 	public function __construct() {
 		$json = $this->createObj($this->PATH_TO_JSON);
+		$this->writeCSV($json);
 	}
 
 	public function createObj(String $pathToJSON) {
 		$stringJson = file_get_contents("priv.json");
 		return json_decode($stringJson, true); // convert to object
+	}
+
+	public function writeCSV(array $json) {
+		print_r($json);
 	}
 }
